cf0d3ba6af1fec6f4ef6815cd964199b8bdb978c
Antonio Fabio <Jorge@DESKTOP-FO69GL4>
2021-04-26 09:42:13 -0300

    Adicionando Interface

---
+++ src/main/java/pacote/InterfaceJavaRMI.java
@@ -0,0 +1,14 @@
+package pacote;
+
+// Todas as interfaces e classes para o sistema de RMI estão no pacote java.rmi
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+//Essa interface funciona como um contrato
+public interface InterfaceJavaRMI extends Remote {
+    String helloWorld(String name) throws RemoteException;
+}
+
+
+//O que é o?
+//Java RMI, ou seja, Remote Method Invocation, é uma API Java usada para implementar chamadas de procedimento remoto (RPC) (chamada de procedimento remoto), que pode transmitir diretamente objetos Java serializados e coleta de lixo distribuída. Sua implementação depende da Java Virtual Machine (JVM), portanto, ela só oferece suporte a chamadas de uma JVM para outra.
